9d4fd519d27ebbc932291f99994a3fd5538a0cae
wangyingtao <wangyingtao@deepin.com>
2017-09-22 14:39:37 +0800

    add command util

---
+++ stacer-core/Utils/command_util.h
@@ -0,0 +1,19 @@
+#ifndef COMMAND_UTIL_H
+#define COMMAND_UTIL_H
+
+#include <QStringList>
+
+#include "stacer-core_global.h"
+
+class STACERCORESHARED_EXPORT CommandUtil
+{
+public:
+    static QString sudoExec(const QString &cmd, QStringList args = QStringList());
+    static QString exec(const QString &cmd, QStringList args = QStringList());
+    static bool isExecutable(const QString &cmd);
+
+private:
+    CommandUtil();
+};
+
+#endif // COMMAND_UTIL_H
