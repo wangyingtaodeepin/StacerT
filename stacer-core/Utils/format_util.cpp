8ce73b50e8181f02c82553728baa676146d4521a
wangyingtao <wangyingtao@deepin.com>
2017-09-26 14:30:58 +0800

    fix compilling error

--- stacer-core/Utils/format_util.cpp
+++ stacer-core/Utils/format_util.cpp
@@ -14,20 +14,20 @@ QString FormatUtil::formatBytes(const quint64 &bytes)
 {
     if (1L == bytes)
         return QString("%1 byte").arg(bytes);
-    else if (bytes < this->KIBI)
-        return QStrint("%1 bytes").arg(bytes);
-    else if (bytes < this->MEBI)
-        return formatUnit(bytes, this->KIBI, "KiB");
-    else if (bytes < this->GIBI)
-        return formatUnit(bytes, this->MEBI, "MiB");
-    else if (bytes < this->TEBI)
-        return formatUnit(bytes, this->GIBI, "GiB");
-    else if (bytes < this->PEBI)
-        return formatUnit(bytes, this->TEBI, "TiB");
-    else if (bytes < this->EXBI)
-        return formatUnit(bytes, this->PEBI, "PiB");
+    else if (bytes < KIBI)
+        return QString("%1 bytes").arg(bytes);
+    else if (bytes < MEBI)
+        return formatUnit(bytes, KIBI, "KiB");
+    else if (bytes < GIBI)
+        return formatUnit(bytes, MEBI, "MiB");
+    else if (bytes < TEBI)
+        return formatUnit(bytes, GIBI, "GiB");
+    else if (bytes < PEBI)
+        return formatUnit(bytes, TEBI, "TiB");
+    else if (bytes < EXBI)
+        return formatUnit(bytes, PEBI, "PiB");
     else
-        return formatUnit(bytes, this->EXBI, "EiB");
+        return formatUnit(bytes, EXBI, "EiB");
 }
 
 
